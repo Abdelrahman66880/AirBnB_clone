a2d0a09c4a37758e65d036a06c20039714c5898f
Abdelrahman <abdofathi317@gmail.com>
2024-02-11 12:20:45 -0800

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+
+"""
+this file include the BaseModel that we will make other classes in herit from it
+they will inherit the attributes and methodes.
+"""
+
+
 import models 
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
+    """the Base class that caoitan methodes to be shared and inherite from it"""
 
     def __init__(self, *args, **kwargs):
-        
+        """Initializes public instance attributes."""
         if kwargs:
             datetime_object = None
             for keys, values in kwargs.items():
@@ -24,6 +32,7 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary representation of the class for JSON serialization."""
 
         exclude = ["my_number", "name"]
         my_dictionary = {}
@@ -37,11 +46,13 @@ class BaseModel:
     
 
     def save(self):
+        """Updates the 'updated_at' attribute to the current datetime and saves changes."""
         self.updated_at = datetime.now()
         models.storage.save()
 
 
     def __str__(self):
+        """Returns a string representation of the class and its attributes."""
         class_name = self.__class__.__name__
 
         return f"[{class_name}] ({self.id}) {self.__dict__}"
